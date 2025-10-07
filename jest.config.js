731a6fd11b3aacc459c08430141d1917843169cd
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2025-10-07 11:27:11 +0200

    Change configuration according node version

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,17 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
- testMatch: ['**/?(*.)+(spec|test).[tj]s'], // ou simplement ['**/*.test.ts', '**/*.spec.ts']
+  testMatch: ['**/?(*.)+(spec|test).[tj]s'], // ou ['**/*.test.ts', '**/*.spec.ts']
   moduleFileExtensions: ['ts', 'js', 'json'],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!tests/**/*.test.ts',
     '!tests/**/*.spec.ts'
-  ]
+  ],
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  }
 };
