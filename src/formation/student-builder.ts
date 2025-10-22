b0bb891e9dfade97513b66f86c00019f57676553
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2025-10-22 11:02:15 +0200

    Student builder implementation

--- src/formation/student-builder.ts
+++ src/formation/student-builder.ts
@@ -1,8 +1,64 @@
 import type { BuilderInterface } from "../core/builder.interface";
-import  { Student } from "../student";
+import  { Student } from "./student";
 
 export class StudentBuilder implements BuilderInterface<Student> {
+    private name: string ='';
+    private firstname: string = '';
+    private email: string = '';
+    private phone: string = '';
+    private birthdate: Date = new Date();
+    private gender: string = '';
+
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+
+
+    public setFirstname(firstname: string): void {
+        this.firstname = firstname;
+    }
+
+
+
+    public setEmail(email: string): void {
+        this.email = email;
+    }
+
+
+
+    public setPhone(phone: string): void {
+        this.phone = phone;
+    }
+
+
+    public setBirthdate(birthdate: Date): void {
+        this.birthdate = birthdate;
+    }
+
+
+    public setGender(gender: string): void {
+        this.gender = gender;
+    }
+
+
     build(): Student {
-        throw new Error("Method not implemented.");
+        if (
+            this.name !== '' &&
+            this.firstname !== '' &&
+            this.email !== '' &&
+            this.phone !== '') {
+                const student = new Student()
+                student.setName(this.name)
+                student.setFirstname(this.firstname)
+                student.setEmail(this.email)
+                student.setPhone(this.phone)
+                student.birthdate = this.birthdate
+                student.gender = this.gender
+
+                return student
+            }
+        throw new Error('Some required attributes are missing')
     }
 }
