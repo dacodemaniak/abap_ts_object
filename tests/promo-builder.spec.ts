37983d948e23a2b2736550ef236576ca7ed8a835
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2025-10-22 13:16:15 +0200

    Add builder and class for promo

---
+++ tests/promo-builder.spec.ts
@@ -0,0 +1,10 @@
+describe('Promo builder test suite', () => {
+
+    it ('Should raised an error if promo is incorrect', () => {
+        expect(true).toBeTruthy()
+    })
+
+    it ('Should get a Promo instance', () => {
+        expect(true).toBe(true)
+    })
+})
